07e8446b90a660dec80b6bcff68ba35127b8fa11
Anoushka <anoushkashanmugham@gmail.com>
2025-12-15 20:21:22 +0530

    Initial commit - Next.js app

---
+++ src/lib/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp,getApps,getApp} from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGyA60k54Dp4JoM2eHl9z0-IpSECDIkMU",
+  authDomain: "ai-study-planner-d2b28.firebaseapp.com",
+  projectId: "ai-study-planner-d2b28",
+  storageBucket: "ai-study-planner-d2b28.firebasestorage.app",
+  messagingSenderId: "812337044236",
+  appId: "1:812337044236:web:a2c2888a6202b827bd8729"
+};
+
+// Initialize Firebase
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const db=getFirestore(app);
